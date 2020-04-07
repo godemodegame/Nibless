2ccba664731dbc5dd854ee7c8fc9460239606845
Кирилл Кириленко <k.kirilenko@lucky.online>
2020-04-07 15:17:11 +0300

    Initial Commit

---
+++ Nibless/Nibless.h
@@ -0,0 +1,19 @@
+//
+//  Nibless.h
+//  Nibless
+//
+//  Created by godemodegame on 07.04.2020.
+//  Copyright © 2020 godemodegame. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Nibless.
+FOUNDATION_EXPORT double NiblessVersionNumber;
+
+//! Project version string for Nibless.
+FOUNDATION_EXPORT const unsigned char NiblessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nibless/PublicHeader.h>
+
+
